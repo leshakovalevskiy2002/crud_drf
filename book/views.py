94e803d445e4aa6c6a2d00726d9198878a74c376
leshakovalevskiy2002 <lesha_programmer02_gt@mail.ru>
2025-10-29 14:52:57 +0300

    Refactor: migrate views to DRF generic views

--- book/views.py
+++ book/views.py
@@ -1,46 +1,15 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookListView(APIView):
-    def get(self, request):
-        books = Book.objects.all()
-        my_serializer = BookSerializer(books, many=True)
-        return Response(my_serializer.data)
-
-    def post(self, request):
-        my_serializer = BookSerializer(data=request.data)
-        if my_serializer.is_valid():
-            my_serializer.save()
-            return Response(my_serializer.data, status=201)
-        return Response(my_serializer.errors, status=400)
+class BookListView(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
-class BookDetailView(APIView):
-    def get_object(self, pk):
-        try:
-            return Book.objects.get(pk=pk)
-        except Book.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        book = self.get_object(pk)
-        serializer = BookSerializer(book)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        book = self.get_object(pk)
-        serializer = BookSerializer(book, request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
-
-    def delete(self, request, pk):
-        book = self.get_object(pk)
-        book.delete()
-        return Response(status=204)
+class BookDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "pk"
